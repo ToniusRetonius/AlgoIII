ba233d66c1fe24f3823a3287a2e1f53792f3c128
Tomas Lopez <toto.melli074@gmail.com>
2024-05-14 19:04:59 -0300

    Parcial 1

--- Entregables/taller6/monocarp.cpp
+++ Entregables/taller6/monocarp.cpp
@@ -17,10 +17,8 @@ int main()
     cin >> t;
 
     /* vectores de diferencias */
-    vector<int> ba;
     vector<int> ab;
-    int impar_ab = 0;
-    int impar_ba = 0; 
+    vector<int> ba;
 
     /* resultados */
     int cambios;
@@ -51,46 +49,32 @@ int main()
     /* si son iguales los string */
     if (ab.size() == 0 && ba.size() == 0)
     {   
+        cout << 0 << endl;
         return 0;
     }
 
-    /* ya tenemos los vectores de diferencias y son no nulos */
-    int tam_ab = ab.size() % 2;
-    int tam_ba = ba.size() % 2;
-
-    if (tam_ab == tam_ba)
-    {   
-        /* caso vectores impares */
-        if (tam_ab == 1)
-        {
-            impar_ab = ab[0];
-            impar_ba = ba[0];
-            
-            ab.erase(ab.begin());
-            ba.erase(ba.begin());
-            
-            res.push_back(impar_ab);
-            res.push_back(impar_ab);
-            res.push_back(impar_ab);
-            res.push_back(impar_ba);
-        }
-
-        /* caso vectores pares */
-        int tam_ab = ab.size() % 2;
-        if (tam_ab == 0 && ab.size() > 1)
-        {   
-            /* juntamos ab con ba y... en caso de res ser != null, anexamos ab total a res */
-            ab.insert(ab.end(), ba.begin(), ba.end());
-            res.insert(res.end(), ab.begin(), ab.end());
-        }
+    /* si es impar enchufamos el ult valor de ab a ambos */
+    if (ab.size() % 2 == 1 )
+    {
+        int ult = ab.back();
+        ab.push_back(ult);
+        ba.push_back(ult);
+    }
+    
+    for (int i = 0; i < ab.size(); i++)
+    {
+        res.push_back(ab[i]);
+    }
+    for (int i = 0; i < ba.size(); i++)
+    {
+        res.push_back(ba[i]);
     }
 
-
-    cambios = res.size() / 2;
-    cout << cambios << endl;
-    for (int i = 0; i < res.size() - 1; i += 2)
+    /* print */
+    cout <<  res.size() / 2 << endl;
+    for (int i = 0; i < res.size(); i = i + 2)
     {
-        cout << res[i] << " " << res[i + 1]<< endl;
+        cout << res[i] << ' ' << res[i + 1] << endl;
     }
     
     return 0;
