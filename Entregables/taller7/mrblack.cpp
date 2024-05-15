912f78a9b9d85f1b38bb9fbfdb6a3a07dfe90f60
ToniusRetonius <you@example.com>
2024-05-14 22:14:00 -0300

    Taller 7

--- Entregables/taller7/mrblack.cpp
+++ Entregables/taller7/mrblack.cpp
@@ -1,3 +1,54 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
+using namespace std;
+
+int main()
+{   
+    /* input r : rooms , d : door, s : switches */
+    int r, d, s;
+    cin >> r >> d >> s;
+
+    /* grafo */
+    vector<vector<int>> habitaciones(d, vector<int>(0));
+    vector<vector<int>> luces(s, vector<int>(0));
+
+    /* res : si tiene un vacio es porq no hay respuesta*/
+    vector<vector<int>> res;
+
+    while (r != 0 && d != 0 && s != 0)
+    {   
+        /* construccion puertas grafo */
+        while (d > 0)
+        {   
+            int i, j;
+            cin >> i >> j;
+            habitaciones[i].push_back(j);
+            habitaciones[j].push_back(i);
+            d--;
+        }
+        /* construccion switches digrafo*/
+        while (s > 0)
+        {
+            int k, l;
+            cin >> k >> l;
+            luces[k].push_back(l);
+            s--;
+        }
+
+        /* vector de luces */
+        vector<int> luces(10, 0);
+        luces[0] = 1;
+
+        /* vector res i auxiliar */
+        vector<int> resi;        
+
+        /* blank */
+        cin.ignore(); 
+        /* siguientes */
+        cin >> r >> d >> s;
+    }
+
+    return 0;
+}
