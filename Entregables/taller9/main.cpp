5c6935f92579e2d90d27537c89b234b235212a9c
Tomas Lopez <toto.melli074@gmail.com>
2024-06-01 09:00:11 -0300

    Taller 9

---
+++ Entregables/taller9/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{   
+    /* declaracion de variables */
+    int casos;
+    int cant_lock;
+    int lock;
+    vector<int> locks;
+    
+    /* capturamos */
+    cin >> casos;
+
+    cin >> cant_lock;
+
+    return 0;
+}
